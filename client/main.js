96269be7ba5754fd98ca000d353ac88c69f4063d
Nathan Villaescusa <theambiguity@gmail.com>
2012-02-25 21:27:08 -0500

    Stuff

--- client/main.js
+++ client/main.js
@@ -1,12 +1,43 @@
 var socket = io.connect('http://localhost');
 
+
 $(function() {
-	$('#user-form form').submit(function() {
-		var username = $('#username', this).val();
+	var $status = $('#status');
+	var $registerForm = $('#join-form');
 
-		socket.emit('register', {username: username});
 
-		alert(username);
+	socket.on('joined', function(user) {
+		addUser(user);
+	});
+
+	socket.on('user-list', function(users) {
+		updateStatus('Connected.');
+
+		for (var i=users.length; i--;) {
+			addUser(users[i]);
+		}
+	});
+
+	$('button', $registerForm).click(function() {
+		var username = $('#username').val();
+		var user = {username: username};
+
+		updateStatus('Connecting to server..')
+		socket.emit('join', user);
+
+		$registerForm.hide();
+		$('#game-area').show();
+
 		return false;
 	});
+
+	function addUser(user) {
+		$('#users-list').append('<li>' + user.username + '</li>');
+	}
+
+	function updateStatus(message) {
+		$status.html(message);
+	}
+
+	updateStatus('Loaded');
 });
